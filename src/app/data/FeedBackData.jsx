88e68859d87a4c5f6ed131dfe642bf5577ebcd0f
Lochmannweb <lochmannweb@gmail.com>
2024-12-06 14:18:48 +0100

    text justering

--- src/app/data/FeedBackData.jsx
+++ src/app/data/FeedBackData.jsx
@@ -3,14 +3,14 @@ export const FeedbackData = {
     Subheader: "If you choose me as your permanent web designer or online frontend developer, you don't have to go and bother with sleepless nights or the heart in the throat.",
     Tech: {
         title: "Technology",
-        feedback: "Jeg er altid på din side, gøre mit bedste for at opnå dine mål, om det er en mere simpel eller med tekniske funktioner som AI",
+        feedback: "I’m always by your side, doing my best to help you achieve your goals—whether they’re simple or involve advanced technical features like AI.",
     },
     Feedback: {
         title: "Feedback",
-        feedback: "Stor tak til dig Line - du tager alle udfordringer med et smil og løser dem med bravur! Well done",
+        feedback: "A big thank you to you, Line — you tackle every challenge with a smile and solve them with brilliance! Well done.",
     },
     Offer: {
         title: "Offer",
-        feedback: "Jeg er studerende og tilbyder dermed mine kunder et unikt tilbud, at bygge unikke design til en unik pris",
+        feedback: "As a student, I offer my clients a unique opportunity: creating one-of-a-kind designs at a one-of-a-kind price.",
     },
 }
