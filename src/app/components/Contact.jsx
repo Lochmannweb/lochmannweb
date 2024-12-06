98feb38d4fff28488d7a0fb20e4efc870fb6d39d
Lochmannweb <lochmannweb@gmail.com>
2024-12-06 13:38:17 +0100

    contact page

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -10,137 +10,172 @@ const breakpoints = {
   lg: '1024px', 
 };
 
-
-const Container = styled.div({
-    display: 'grid',
-    gap: '10px',
-    filter: 'drop-shadow(0 3mm 4mm #000)',
-    borderRadius: '10px',
-    // marginTop: '10rem',
-    [`@media (min-width: ${breakpoints.md})`]: {
-      borderRadius: '25px',
-      // paddingTop: '4rem',
-      paddingLeft: '3rem',
-      paddingRight: '3rem',
-      // marginTop: '-5rem',
-      width: '100%',
-      margin: 'auto',
-    }
-});
-
-const Header = styled.div({
-  fontSize: '18px',
-  textAlign: 'start',
-  fontFamily: '"Keania One", sans-serif',
-  [`@media (min-width: ${breakpoints.md})`]: {
-    fontSize: '30px',
-    textAlign: 'center',
-  },
-});
-
-const Subheader = styled.div({
-  color: '#aaa',
-  fontSize: '12px',
-  paddingBottom: '1rem',
-  marginTop: '-1rem',
-  [`@media (min-width: ${breakpoints.md})`]: {
-    textAlign: 'center',
-    fontSize: '12px',
-  },
-});
-
-const Form = styled.div({
-  marginBottom: '1rem',
-  [`@media (min-width: ${breakpoints.md})`]: {
-    marginBottom: '1rem',
-  },
-});
-
-const Label = styled.div({
-  fontSize: '10px',
-  marginLeft: '5px',
-  fontFamily: '"Keania One", sans-serif',
-  [`@media (min-width: ${breakpoints.md})`]: {
-    fontSize: '15px',
-  },
-});
-
-const ThankYouForJoining = styled.div({
-  fontSize: '20px',
-  fontFamily: '"Keania One", sans-serif',
-  margin: 'auto',
-  padding: '2rem',
-  [`@media (min-width: ${breakpoints.md})`]: {
-    fontSize: '40px',
-    padding: '5rem',
-  },
-});
-
-const BoxContainer = styled.div({
-  [`@media (min-width: ${breakpoints.md})`]: {
-  },
-});
-
-const BorderInput = styled.div({
-});
-
-function App() {
+function ContactForm() {
   const [state, handleSubmit] = useForm("mldrldzl");
   if (state.succeeded) {
-      return <ThankYouForJoining>I look forward to see your vision!</ThankYouForJoining>;
+      return <p className='font-keania md:text-2xl m-auto md:r-44 md:mt-48'>Looking forward to work with you!</p>;
   }
-
   return (
-    <>
-    <BoxContainer>
-       <Container>
-           <Header>{ContactFormularData.title}</Header>
-           <Subheader>{ContactFormularData.title2}</Subheader>
-           <form onSubmit={handleSubmit}>
-             <Form>
-               <Label htmlFor="text">{ContactFormularData.companyName}</Label>
-               <BorderInput>
-                  <input 
-                    className='w-full bg-input rounded-md text-xs p-2' 
-                    name="Text" 
-                    id="text" 
-                    type="text" 
-                    placeholder='If the website exist'>
-                  </input>
-               </BorderInput>
-               <ValidationError prefix="Text" field="text" errors={state.errors} />
-             </Form>
-   
-             <Form> 
-               <Label htmlFor="text">{ContactFormularData.fullName}</Label>
-               <BorderInput><input className='w-full rounded-md bg-input text-xs p-2' name="Text" id="text" type="text" placeholder='John James'></input></BorderInput>
-               <ValidationError prefix="Text" field="text" errors={state.errors} />
-             </Form>
-   
-             <Form> 
-               <Label htmlFor="tel">{ContactFormularData.phonenr}</Label>
-               <BorderInput><input className='w-full bg-input rounded-md text-xs p-2' name="Tel" id="tel" type="tel" placeholder='+xx xx xx xx xx'></input></BorderInput>
-               <ValidationError prefix="Tel" field="tel" errors={state.errors} />
-             </Form>
-   
-             <Form>
-               <Label htmlFor="email">{ContactFormularData.mailadress}</Label>
-               <BorderInput><input className='w-full bg-input rounded-md text-xs p-2' name="Email" id="email" type="email" placeholder='example@example.com'></input></BorderInput>
-               <ValidationError prefix="Email" field="email" errors={state.errors} />
-             </Form>
-   
-             <Form>
-               <Label htmlFor="message">{ContactFormularData.message}</Label>
-               <BorderInput><input className='w-full bg-input rounded-md text-xs p-2 h-20' name="Message" id="message" type="message" placeholder=''></input></BorderInput>
-               <ValidationError prefix="Message" field="message" errors={state.errors} />
-             </Form>
-           </form>
-       </Container>
-       <button className='flex p-1 items-center rounded-2xl border-2 m-auto mb-5 w-44 text-base justify-center font-keania' type="submit" disabled={state.submitting}>{ContactFormularData.button}</button>
+    <form className='grid gap-4 md:mt-20' onSubmit={handleSubmit}>
 
-    </BoxContainer>
-    </>
+      <div className='grid'>
+        <label htmlFor="text" className='font-keania'>
+          Firma navn
+        </label>
+        <input
+          id="text"
+          type="text" 
+          name="text"
+          className='bg-input text-white text-xs p-2'
+          placeholder='If the website exist'
+        />
+        <ValidationError 
+          prefix="Text" 
+          field="text"
+          errors={state.errors}
+        />
+      </div>
+
+      <div className='grid'>
+        <label htmlFor="text" className='font-keania'>
+          Full name
+        </label>
+        <input
+          id="text"
+          type="text" 
+          name="text"
+          className='bg-input text-white text-xs p-2'
+          placeholder='John James'
+        />
+        <ValidationError 
+          prefix="Text" 
+          field="text"
+          errors={state.errors}
+        />
+      </div>
+
+      <div className='grid'>
+        <label htmlFor="tel" className='font-keania'>
+          Phone number
+        </label>
+        <input
+          id="tel"
+          type="tel" 
+          name="tel"
+          className='bg-input text-white text-xs p-2'
+          placeholder='+XX XX XX XX XX'
+        />
+        <ValidationError 
+          prefix="Tel" 
+          field="tel"
+          errors={state.errors}
+        />
+      </div>
+
+      <div className='grid'>
+        <label htmlFor="email" className='font-keania'>
+          Email Address
+        </label>
+        <input
+          id="email"
+          type="email" 
+          name="email"
+          className='bg-input text-white text-xs p-2'
+          placeholder='example@example.com'
+        />
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
+      </div>
+
+      <div className='grid'>
+        <label htmlFor="message" className='font-keania'>
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className='bg-input text-white text-xs p-2'
+        />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+
+      <div>
+        <button type="submit" className='font-keania' disabled={state.submitting}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <ContactForm />
   );
 }
 
 export default App;
+
+
+// function Contactform() {
+//   const [state, handleSubmit] = useForm("mldrldzl");
+//   if (state.succeeded) {
+//       return <ThankYouForJoining>I look forward to see your vision!</ThankYouForJoining>;
+//   }
+
+//   return (
+//     <>
+//        <Container>
+//            <Header>{ContactFormularData.title}</Header>
+//            <Subheader>{ContactFormularData.title2}</Subheader>
+//            <form onSubmit={handleSubmit}>
+//              <Form>
+//                <Label><label htmlFor="text">{ContactFormularData.companyName}</label></Label>
+//                <BorderInput><input className='w-full bg-input rounded-md text-xs p-2' name="text" id="text" type="text" placeholder='If the website exist'></input></BorderInput>
+//                <ValidationError prefix="Text" field="text" errors={state.errors} />
+//              </Form>
+   
+//              <Form> 
+//              <Label><label htmlFor="text">{ContactFormularData.fullName}</label></Label>
+//                <BorderInput><input className='w-full rounded-md bg-input text-xs p-2' name="text" id="text" type="text" placeholder='John James'></input></BorderInput>
+//                <ValidationError prefix="Text" field="text" errors={state.errors} />
+//              </Form>
+   
+//              <Form> 
+//                <Label><label htmlFor="tel">{ContactFormularData.phonenr}</label></Label>
+//                <BorderInput><input className='w-full bg-input rounded-md text-xs p-2' name="tel" id="tel" type="tel" placeholder='+xx xx xx xx xx'></input></BorderInput>
+//                <ValidationError prefix="Tel" field="tel" errors={state.errors} />
+//              </Form>
+   
+//              <Form>
+//              <Label><label htmlFor="email">{ContactFormularData.mailadress}</label></Label>
+//                <BorderInput><input className='w-full bg-input rounded-md text-xs p-2' name="email" id="email" type="email" placeholder='example@example.com'></input></BorderInput>
+//                <ValidationError prefix="Email" field="email" errors={state.errors} />
+//              </Form>
+   
+//              <Form>
+//              <Label><label htmlFor="message">{ContactFormularData.message}</label></Label>
+//                <BorderInput><textarea className='w-full bg-input rounded-md text-xs p-2 h-20' name="message" id="message"></textarea></BorderInput>
+//                <ValidationError prefix="Message" field="message" errors={state.errors} />
+//              </Form>
+//            </form>
+//        </Container>
+//        <button className='flex p-1 items-center rounded-2xl border-2 m-auto mb-5 w-44 text-base justify-center font-keania' type="submit" disabled={state.submitting}>{ContactFormularData.button}</button>
+//     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Contactform />
+//   );
+// }
+
+// export default App;
